72dca42862a026de12c3deb1f847fbdeddfcb6a1
Rohit-Rishishwar1312 <rishishwarrohit@gmail.com>
2020-08-18 18:47:55 +0530

    mywebsite

---
+++ get.js
@@ -0,0 +1,11 @@
+var element;
+
+//element =document.body;
+//console.log(element);
+//element=document.getElementById("rohit").innerText;
+//element=document.getElementById("rohit").innerHTML;
+//element=document.getElementById("rohit").getAttribute("style");
+//element=document.getElementById("rohit").getAttributeNode("style");
+//element=document.getElementById("rohit").attributes;
+element=document.getElementById("rohit").attributes[0];
+console.log(element);
---
+++ set.js
@@ -0,0 +1,9 @@
+//document.getElementById("rohit").innerHTML="<h1>Bye Rohit</h1>"
+//document.getElementById("rohit").setAttribute("style","border: 4px dotted yellow");
+//document.getElementById("rohit").removeAttribute("style");
+/// query selector set or get dono///
+//document.querySelector("#rohit").innerHTML="<h1>Wow Mohit Rohit</h1>";
+//var element;
+//element = document.querySelector("#rohit");
+//console.log(element);
+
